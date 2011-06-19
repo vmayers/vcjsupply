e7f643341a9e4eaed002c721a461518c561203ff
Vincent Mayers <vmayers@gmail.com>
2011-06-19 09:11:15 -0500

    Polished the Contact and About content

--- app/controllers/static_page_contents_controller.rb
+++ app/controllers/static_page_contents_controller.rb
@@ -4,14 +4,14 @@ class StaticPageContentsController < ApplicationController
   
   uses_tiny_mce
   
-  def index
-    @static_page_contents = StaticPageContent.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @static_page_contents }
-    end
-  end
+  # def index
+  #     @static_page_contents = StaticPageContent.all
+  # 
+  #     respond_to do |format|
+  #       format.html # index.html.erb
+  #       format.xml  { render :xml => @static_page_contents }
+  #     end
+  #   end
 
   # GET /static_page_contents/1
   # GET /static_page_contents/1.xml
@@ -37,14 +37,14 @@ class StaticPageContentsController < ApplicationController
 
   # GET /static_page_contents/new
   # GET /static_page_contents/new.xml
-  def new
-    @static_page_content = StaticPageContent.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @static_page_content }
-    end
-  end
+  # def new
+  #    @static_page_content = StaticPageContent.new
+  # 
+  #    respond_to do |format|
+  #      format.html # new.html.erb
+  #      format.xml  { render :xml => @static_page_content }
+  #    end
+  #  end
 
   # GET /static_page_contents/1/edit
   def edit_contact
@@ -59,19 +59,19 @@ class StaticPageContentsController < ApplicationController
 
   # POST /static_page_contents
   # POST /static_page_contents.xml
-  def create
-    @static_page_content = StaticPageContent.new(params[:static_page_content])
-
-    respond_to do |format|
-      if @static_page_content.save
-        format.html { redirect_to(@static_page_content, :notice => 'Static page content was successfully created.') }
-        format.xml  { render :xml => @static_page_content, :status => :created, :location => @static_page_content }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @static_page_content.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+  # def create
+  #     @static_page_content = StaticPageContent.new(params[:static_page_content])
+  # 
+  #     respond_to do |format|
+  #       if @static_page_content.save
+  #         format.html { redirect_to(@static_page_content, :notice => 'Static page content was successfully created.') }
+  #         format.xml  { render :xml => @static_page_content, :status => :created, :location => @static_page_content }
+  #       else
+  #         format.html { render :action => "new" }
+  #         format.xml  { render :xml => @static_page_content.errors, :status => :unprocessable_entity }
+  #       end
+  #     end
+  #   end
 
   # PUT /static_page_contents/1
   # PUT /static_page_contents/1.xml
@@ -91,13 +91,13 @@ class StaticPageContentsController < ApplicationController
 
   # DELETE /static_page_contents/1
   # DELETE /static_page_contents/1.xml
-  def destroy
-    @static_page_content = StaticPageContent.find(params[:id])
-    @static_page_content.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(static_page_contents_url) }
-      format.xml  { head :ok }
-    end
-  end
+  # def destroy
+  #     @static_page_content = StaticPageContent.find(params[:id])
+  #     @static_page_content.destroy
+  # 
+  #     respond_to do |format|
+  #       format.html { redirect_to(static_page_contents_url) }
+  #       format.xml  { head :ok }
+  #     end
+  #   end
 end
